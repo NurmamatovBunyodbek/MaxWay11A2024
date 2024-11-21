fcc90c83698c4c8e3ca6cd9af7328c2078539454
Bunyodbek Nurmamatov <bunyodbeknurmamatov@gmail.com>
2024-11-21 16:45:19 +0500

    add inline menu

--- src/main/java/uz/bunyodbek/MyBot.java
+++ src/main/java/uz/bunyodbek/MyBot.java
@@ -1,6 +1,8 @@
 package uz.bunyodbek;
 
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.updatingmessages.EditMessageReplyMarkup;
+import org.telegram.telegrambots.meta.api.objects.CallbackQuery;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
@@ -20,6 +22,54 @@ public class MyBot extends TelegramLongPollingBot {
                 try {
                     execute(myBotService.sendMessage(chatId));
                     execute(myBotService.menu(chatId));
+                } catch (TelegramApiException e) {
+                    throw new RuntimeException(e);
+                }
+
+            }
+            if (text.equals("ShoxMed kartasi")) {
+                try {
+                    execute(myBotService.shohmedmenu(chatId));
+                } catch (TelegramApiException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+
+
+        } else if (update.hasCallbackQuery()) {
+            CallbackQuery callbackQuery = update.getCallbackQuery();
+            String data = callbackQuery.getData();
+            Long chatId = callbackQuery.getMessage().getChatId();
+
+            Integer messageId = callbackQuery.getMessage().getMessageId();
+
+            if (data.equals("uzbekid")) {
+                try {
+                    execute(myBotService.mainMenu(chatId));
+                } catch (TelegramApiException e) {
+                    throw new RuntimeException(e);
+                }
+
+            }
+            if (data.equals("tafsilotId")) {
+                EditMessageReplyMarkup editMessageReplyMarkup = new EditMessageReplyMarkup();
+                editMessageReplyMarkup.setChatId(chatId);
+                editMessageReplyMarkup.setMessageId(messageId);
+                editMessageReplyMarkup.setReplyMarkup(myBotService.tafsilotMenu(chatId));
+                try {
+                    execute(editMessageReplyMarkup);
+                } catch (TelegramApiException e) {
+                    throw new RuntimeException(e);
+                }
+
+            }
+            if (data.equals("tafsilotlarqaytishId")) {
+                EditMessageReplyMarkup editMessageReplyMarkup = new EditMessageReplyMarkup();
+                editMessageReplyMarkup.setChatId(chatId);
+                editMessageReplyMarkup.setMessageId(messageId);
+                editMessageReplyMarkup.setReplyMarkup(myBotService.shohmedmenuedit(chatId));
+                try {
+                    execute(editMessageReplyMarkup);
                 } catch (TelegramApiException e) {
                     throw new RuntimeException(e);
                 }
--- src/main/java/uz/bunyodbek/MyBotService.java
+++ src/main/java/uz/bunyodbek/MyBotService.java
@@ -2,30 +2,31 @@ package uz.bunyodbek;
 
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardButton;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class MyBotService {
 
-    public SendMessage sendMessage(Long chatId){
+    public SendMessage sendMessage(Long chatId) {
         SendMessage sendMessage = new SendMessage();
         sendMessage.setChatId(chatId);
         sendMessage.setText(" Shox Med ga xush kelibsiz");
         return sendMessage;
     }
 
-    public SendMessage menu(Long chatId){
+    public SendMessage menu(Long chatId) {
         SendMessage sendMessage = new SendMessage();
         sendMessage.setChatId(chatId);
         sendMessage.setText("Tanlang");
 
-
         InlineKeyboardMarkup inlineKeyboardMarkup = new InlineKeyboardMarkup();
         List<InlineKeyboardButton> row = new ArrayList<>();
         List<List<InlineKeyboardButton>> rowList = new ArrayList<>();
-
 
         InlineKeyboardButton button = new InlineKeyboardButton();
         button.setText("Rus tili");
@@ -33,15 +34,15 @@ public class MyBotService {
         row.add(button);
         rowList.add(row);
 
-        row= new ArrayList<>();
-        button= new InlineKeyboardButton();
+        row = new ArrayList<>();
+        button = new InlineKeyboardButton();
         button.setText("O'zbek tili");
         button.setCallbackData("uzbekid");
         row.add(button);
         rowList.add(row);
 
-        row= new ArrayList<>();
-        button= new InlineKeyboardButton();
+        row = new ArrayList<>();
+        button = new InlineKeyboardButton();
         button.setText("English");
         button.setCallbackData("englishid");
         row.add(button);
@@ -51,14 +52,145 @@ public class MyBotService {
         sendMessage.setReplyMarkup(inlineKeyboardMarkup);
         return sendMessage;
 
+    }
 
 
+    public SendMessage mainMenu(Long chatId) {
+        SendMessage sendMessage = new SendMessage();
+        sendMessage.setChatId(chatId);
+        sendMessage.setText("Tanlang");
 
+        ReplyKeyboardMarkup replyKeyboardMarkup = new ReplyKeyboardMarkup();
+        List<KeyboardRow> rowList = new ArrayList<>();
+
+        KeyboardRow row = new KeyboardRow();
+        KeyboardButton button = new KeyboardButton();
+        button.setText("ShoxMed kartasi");
+        row.add(button);
+        rowList.add(row);
+
+        replyKeyboardMarkup.setKeyboard(rowList);
+        replyKeyboardMarkup.setResizeKeyboard(true);
+        sendMessage.setReplyMarkup(replyKeyboardMarkup);
+        return sendMessage;
 
 
     }
 
+    public SendMessage shohmedmenu(Long chatId) {
 
+        SendMessage sendMessage = new SendMessage();
+        sendMessage.setChatId(chatId);
+        sendMessage.setText("Marhamat!");
+
+        InlineKeyboardMarkup inlineKeyboardMarkup = new InlineKeyboardMarkup();
+        List<InlineKeyboardButton> row = new ArrayList<>();
+        List<List<InlineKeyboardButton>> rowList = new ArrayList<>();
+
+        InlineKeyboardButton button = new InlineKeyboardButton();
+        button.setText("Tafsilotlar");
+        button.setCallbackData("tafsilotId");
+        row.add(button);
+        rowList.add(row);
+
+        row = new ArrayList<>();
+        button = new InlineKeyboardButton();
+
+        button.setText("Elektron karta");
+        button.setCallbackData("elektronId");
+        row.add(button);
+        rowList.add(row);
+
+
+        row = new ArrayList<>();
+        button = new InlineKeyboardButton();
+
+        button.setText("qaytish");
+        button.setCallbackData("qaytishId");
+        row.add(button);
+        rowList.add(row);
+
+        inlineKeyboardMarkup.setKeyboard(rowList);
+        sendMessage.setReplyMarkup(inlineKeyboardMarkup);
+        return sendMessage;
+
+    }
+
+    public InlineKeyboardMarkup shohmedmenuedit(Long chatId) {
+
+        SendMessage sendMessage = new SendMessage();
+        sendMessage.setChatId(chatId);
+        sendMessage.setText("Marhamat!");
+
+        InlineKeyboardMarkup inlineKeyboardMarkup = new InlineKeyboardMarkup();
+        List<InlineKeyboardButton> row = new ArrayList<>();
+        List<List<InlineKeyboardButton>> rowList = new ArrayList<>();
+
+        InlineKeyboardButton button = new InlineKeyboardButton();
+        button.setText("Tafsilotlar");
+        button.setCallbackData("tafsilotId");
+        row.add(button);
+        rowList.add(row);
+
+        row = new ArrayList<>();
+        button = new InlineKeyboardButton();
+
+        button.setText("Elektron karta");
+        button.setCallbackData("elektronId");
+        row.add(button);
+        rowList.add(row);
+
+
+        row = new ArrayList<>();
+        button = new InlineKeyboardButton();
+
+        button.setText("qaytish");
+        button.setCallbackData("qaytishId");
+        row.add(button);
+        rowList.add(row);
+
+        inlineKeyboardMarkup.setKeyboard(rowList);
+        sendMessage.setReplyMarkup(inlineKeyboardMarkup);
+        return inlineKeyboardMarkup;
+
+    }
+
+    public InlineKeyboardMarkup tafsilotMenu(Long chatId) {
+        SendMessage sendMessage = new SendMessage();
+        sendMessage.setChatId(chatId);
+        sendMessage.setText("Marhamat!");
+
+        InlineKeyboardMarkup inlineKeyboardMarkup = new InlineKeyboardMarkup();
+
+        List<InlineKeyboardButton> row = new ArrayList<>();
+        List<List<InlineKeyboardButton>> rowList = new ArrayList<>();
+
+        InlineKeyboardButton button = new InlineKeyboardButton();
+        button.setText("Elektron kartani tiklash");
+        button.setCallbackData("kartatiklashId");
+        row.add(button);
+        rowList.add(row);
+
+
+        row = new ArrayList<>();
+        button = new InlineKeyboardButton();
+
+        button.setText("Qoidalar");
+        button.setCallbackData("qoidalarid");
+        row.add(button);
+        rowList.add(row);
+
+        row = new ArrayList<>();
+        button = new InlineKeyboardButton();
+        button.setText("Qaytish");
+        button.setCallbackData("tafsilotlarqaytishId");
+        row.add(button);
+        rowList.add(row);
+
+        inlineKeyboardMarkup.setKeyboard(rowList);
+        sendMessage.setReplyMarkup(inlineKeyboardMarkup);
+        return inlineKeyboardMarkup;
+    }
 
 
 }
